f4d1354ccd19e7c683d55cfd79b1448f89b37724
Nikhilesh Belulkar <nb2953@columbia.edu>
2023-02-25 18:54:34 -0500

    pushing the stuff we need

---
+++ submit_website_to_tx.py
@@ -0,0 +1,22 @@
+from utilities import zip_webpage, encode_webpage, decoded_webpage, unzip_webpage
+from submit_transaction import connect_to_client, check_balance, build_transaction, submit_transction
+
+def sumbit_webpage(folder):
+    
+
+
+    pass
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
