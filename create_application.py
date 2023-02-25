16e235c5a45daed021f33d48350dbd7bacf7ac45
Sai Chintalapati <vhc2109@columbia.edu>
2023-02-25 06:56:31 -0500

    improved and fixed issues

--- create_application.py
+++ create_application.py
@@ -2,27 +2,26 @@ from pyteal import *
 from beaker import sandbox
 from base64 import b64decode
 import algosdk
-from algosdk.transaction import ApplicationCreateTxn, StateSchema, wait_for_confirmation
+from algosdk.transaction import ApplicationCreateTxn, ApplicationCallTxn, StateSchema, wait_for_confirmation
 
 client = sandbox.get_algod_client()
 accounts = sandbox.get_accounts()
 
 def approval_program():
     number = Bytes("number")
-
-    init = Seq(App.globalPut(number, Int(0)), Approve())
-
-    store = Seq(App.globalPut(number, Btoi(Txn.application_args[0])), Approve())
-
+    init = Seq(
+        [
+            App.globalPut(number, Int(0)), Approve()
+        ]
+    )
+    create = Seq(
+        Pop(App.box_create(Bytes("testbox"), Int(100))), Approve()
+    )
     program = Cond(
         [Txn.application_id() == Int(0), init],
-        [Txn.on_completion() == OnComplete.NoOp, store],
-        [Txn.on_completion() == OnComplete.DeleteApplication, Reject()],
-        [Txn.on_completion() == OnComplete.UpdateApplication, Reject()],
-        [Txn.on_completion() == OnComplete.OptIn, Reject()],
-        [Txn.on_completion() == OnComplete.CloseOut, Reject()],
+        [Txn.on_completion() == OnComplete.NoOp, create],
     )
-    return compileTeal(program, mode=Mode.Application, version=6)
+    return compileTeal(program, mode=Mode.Application, version=8)
 
 
 def compile_program(client, source_code):
@@ -34,7 +33,7 @@ def compile_program(client, source_code):
 clear_state_program = compileTeal(
     Approve(),
     mode=Mode.Application,
-    version=6
+    version=8
 )
 
 signer = accounts[0]
@@ -46,11 +45,26 @@ txn = ApplicationCreateTxn(
     clear_program=compile_program(client, clear_state_program),
     global_schema=StateSchema(num_uints=1, num_byte_slices=1),
     local_schema=StateSchema(num_uints=0, num_byte_slices=0),
+    boxes=[(0, b"testbox")]
 )
+
+print(txn)
 
 signedTxn = txn.sign(signer.private_key)
 txid = client.send_transaction(signedTxn)
 response = wait_for_confirmation(client, signedTxn.get_txid(), 4)
 print(f"Created App with id: {response['application-index']}  in tx: {txid}")
 
+app_id = response['application-index']
+txn = ApplicationCallTxn(
+    sender=accounts[0].address,
+    sp=client.suggested_params(),
+    index=app_id,
+    on_complete=algosdk.transaction.OnComplete.NoOpOC,
+    app_args=[5],   
+    boxes=[(0, b"testbox")]
+)
 
+signedTxn = txn.sign(accounts[0].private_key)
+txid = client.send_transaction(signedTxn)
+response = wait_for_confirmation(client, signedTxn.get_txid(), 4)
